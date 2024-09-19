143387b70a1d8370860f34a49867b827b4a135ff
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-09-19 23:45:28 +0300

    Add pushing to git repository builds

--- scripts/linux-x64_build.sh
+++ scripts/linux-x64_build.sh
@@ -1,3 +1,11 @@
 cd ./db_app
 
 dotnet publish -c Release -r linux-x64 --self-contained
+
+cd ..
+
+git add .
+
+git commit -m "Linux-x64 build"
+
+git push
--- scripts/win-x64_build.sh
+++ scripts/win-x64_build.sh
@@ -1,3 +1,11 @@
 cd ./db_app
 
 dotnet publish -c Release -r win-x64 --self-contained
+
+cd ..
+
+git add .
+
+git commit -m "Win-x64 build"
+
+git push
