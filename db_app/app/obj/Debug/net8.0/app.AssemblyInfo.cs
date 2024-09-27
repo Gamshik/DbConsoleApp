9cf8fd3173154b31b969f479c95bc907e2f32fc5
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-09-27 11:05:07 +0300

    Update proj

--- db_app/app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ db_app/app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c74afd5d80efaf0937343277da3416f8e4748a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597e41701ead22673c7346d3a6934abf43c05fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
